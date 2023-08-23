50c278775a7f1e11a3028b8a24f9785a4ded8541
matscoder <levin.hsu@outlook.com>
2023-08-23 10:19:32 +0800

    黑马程序员c++第一天，到p9

---
+++ 第一节课-书写c++/书写c++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*int   main()
+{
+	cout << "Hello world" << endl;
+
+	system("pause");
+
+	return 0;
+}    */
+
+
+/*int main()
+{
+	int a = 10;   
+	cout << "a=" << a << endl;
+
+	system("pause");
+
+	return 0;
+}*/
+  
+
+#define day 7
+int main()
+{
+	const int month = 12;
+
+		cout << "一周总共有：" << day << "天" << endl;
+		cout << "一年总共有：" << month << "个月份" << endl;
+
+		    
+		system("pause");
+	return 0;
+}
